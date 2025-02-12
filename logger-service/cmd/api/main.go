4013e5e0153e7390430b1d29641a065cfc7164b1
Vincent Lin <v2266514@gmail.com>
2025-02-13 03:38:42 +0800

    feat: feat: switch log push from API to RPC

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"log-service/data"
+	"net"
 	"net/http"
+	"net/rpc"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -49,19 +51,13 @@ func main() {
 		Models: data.New(client),
 	}
 
+	// Register the RPC Server
+	err = rpc.Register(new(RPCServer))
+	go app.rpcListen()
+
 	// start web server
 	log.Println("Starting service on port", webPort)
 	app.serve()
-	// srv := &http.Server{
-	// 	Addr:    fmt.Sprintf(":%s", webPort),
-	// 	Handler: app.routes(),
-	// }
-
-	// err = srv.ListenAndServe()
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
-
 }
 
 func (app *Config) serve() {
@@ -73,6 +69,23 @@ func (app *Config) serve() {
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
+	}
+}
+
+func (app *Config) rpcListen() error {
+	log.Println("Starting RPC server on port ", rpcPort)
+	listen, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", rpcPort))
+	if err != nil {
+		return err
+	}
+	defer listen.Close()
+
+	for {
+		rpcConn, err := listen.Accept()
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(rpcConn)
 	}
 }
 
